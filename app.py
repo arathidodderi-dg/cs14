9c54c7f22e6eea1c7a2687d3245244fde799249a
arathi <arathidodderi@gmail.com>
2026-08-22 17:28:16 +0530

    aff

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ def name_intake(name):
     print(name)
 name=input("enter your name:")
 name_intake(name)
+print(x)
+x=input("enter your 1st name:")
+print(y)
+y=input("enter 2nd name:")
+
 
 def sumit(a,b):
     return a+b
